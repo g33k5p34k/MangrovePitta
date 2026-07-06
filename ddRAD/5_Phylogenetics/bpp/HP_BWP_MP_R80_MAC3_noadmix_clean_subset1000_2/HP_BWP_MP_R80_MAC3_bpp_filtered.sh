26f1e186de6c61680fbe852d7892209a4f88f37c
David Tan <david.tanjx@gmail.com>
2026-07-07 01:28:05 +0200

    Add files via upload

---
+++ ddRAD/5_Phylogenetics/bpp/HP_BWP_MP_R80_MAC3_noadmix_clean_subset1000_2/HP_BWP_MP_R80_MAC3_bpp_filtered.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=bpp_R80_filtered_subset1
+#SBATCH --output=bpp_R80_filtered_subset1.txt
+#SBATCH --account=2016005
+#SBATCH --mail-user=davidtan@unm.edu
+#SBATCH --mail-type=ALL
+#
+#SBATCH --ntasks=4
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=96000
+#SBATCH --partition=bigmem-1TB
+#
+############################################
+
+module load miniconda3
+
+## start bpp environment
+
+source activate bpp_env
+
+cd /carc/scratch/projects/andersen2016005/pitta/ddRAD/5_Phylogenetics/HP_BWP_MP_R80_MAC3_noadmix_clean_subset1000_2/
+
+~/bpp/src/bpp --cfile /carc/scratch/projects/andersen2016005/pitta/ddRAD/5_Phylogenetics/HP_BWP_MP_R80_MAC3_noadmix_clean_subset1000_2/HP_BWP_MP_R80_MAC3_phylip_bpp_filtered.ctl
