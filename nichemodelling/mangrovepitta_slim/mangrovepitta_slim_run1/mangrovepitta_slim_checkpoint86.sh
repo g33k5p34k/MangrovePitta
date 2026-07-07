7d192fa152619a3372aeba6ae81658ad2661fde9
David Tan <david.tanjx@gmail.com>
2026-07-07 13:57:38 +0200

    Add files via upload

---
+++ nichemodelling/mangrovepitta_slim/mangrovepitta_slim_run1/mangrovepitta_slim_checkpoint86.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mangrovepitta_slim
+#SBATCH --output=mangrovepitta_slim.txt
+#SBATCH --account=2016005
+#SBATCH --mail-user=davidtan@unm.edu
+#SBATCH --mail-type=ALL
+#
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8000
+#SBATCH --partition=normal
+#
+############################################
+
+module load miniconda3
+
+## start SLiM environment
+
+source activate slim4_env
+
+cd /carc/scratch/projects/andersen2016005/pitta/nichemodelling/mangrovepitta_slim/mangrovepitta_slim_run1/
+
+slim /carc/scratch/projects/andersen2016005/pitta/nichemodelling/mangrovepitta_slim/mangrovepitta_slim_run1/mp_slim_checkpoint86.txt
